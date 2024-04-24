1120e5d34fd916396bb71eb6f448ef1b5377a733
Babakaaz <Babakaaz@users.noreply.github.com>
2024-04-24 22:45:30 +0430

    added the navbar to the index

--- view/index.php
+++ view/index.php
@@ -4,29 +4,29 @@ require_once "../model/user.php";
 require_once "../model/pdo.php";
 $username = false;
 if (($userid = loggedin()) !== false) {
-    if (($row = userid2username($dbh, $userid)) != false) {
-        $username = $row["username"];
-    }
+	if (($row = userid2username($dbh, $userid)) != false) {
+		$username = $row["username"];
+	}
 }
 
-if(isset($_GET['search'])){
-    $db = new PDO('sqlite:db.db');
-    $searchTerm = $_GET['search'];
-    $query = "SELECT * FROM categories WHERE categoryname LIKE '%$searchTerm%'";
-    $result = $db->query($query);
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)){
-        echo '<div class="result">';
-        echo '<h3>' . $row['categoryname'] . '</h3>';
-        $categoryId = $row['categoryid'];
-        $productsQuery = "SELECT * FROM categories WHERE categoryid = $categoryId";
-        $productsResult = $db->query($productsQuery);
-        while ($product = $productsResult->fetch(PDO::FETCH_ASSOC))
-        {
-            echo '<p>' . $product['categoryname'] . '</p>';
-        }
-        echo '</div>';
-    }
-    $db = null;
+if(isset($_GET['s'])){
+	$db = new PDO('sqlite:db.db');
+	$searchTerm = $_GET['search'];
+	$query = "SELECT * FROM categories WHERE categoryname LIKE '%$searchTerm%'";
+	$result = $db->query($query);
+	while ($row = $result->fetch(PDO::FETCH_ASSOC)){
+		echo '<div class="result">';
+		echo '<h3>' . $row['categoryname'] . '</h3>';
+		$categoryId = $row['categoryid'];
+		$productsQuery = "SELECT * FROM categories WHERE categoryid = $categoryId";
+		$productsResult = $db->query($productsQuery);
+		while ($product = $productsResult->fetch(PDO::FETCH_ASSOC))
+		{
+			echo '<p>' . $product['categoryname'] . '</p>';
+		}
+		echo '</div>';
+	}
+	$db = null;
 }
 ?>
 <!DOCTYPE html>
@@ -40,144 +40,121 @@ if(isset($_GET['search'])){
     <link rel="stylesheet" href="var.css">
     <link rel="stylesheet" href="tap.bar footer.css">
     <link rel="stylesheet" href="search.php">
+		<link rel="stylesheet" href="./navbar/style.css" />
 </head>
 
 <body>
-    <div class="container">
-        <div class="top-bar">
-            <div class="left-items">
-                <?php if ($username == false) {
-                    echo '<a class="btn" href="sign-up.php">ورود / ثبت نام </a>';
-                } else {
-                    $husername = htmlentities($username);
-                    echo '<a class="btn" href="userpage/?=username"' . $husername . '>' . $husername . '</a>';
-                } ?>
+<?php require "./navbar/nav.php";?>
+	<!-- <div class="second-row">
+	    <a href="#" class="nav-item">دسته بندی</a>
+	    <a href="#" class="nav-item">پیگیری سفارشات</a>
+	    <a href="#" class="nav-item">درباره ما</a>
+	</div> -->
+	<img style="width: 100vw;    height: auto;    margin-top: 20px;" src="picture/1.jpg" alt="">
+	<div class="categories" class="amir" style="margin-top: 50px; ">
+	    <div class="category">
+		<a href="category1.html">
+		    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
+		    <p>هنر</p>
+		</a>
+	    </div>
+	    <div class="category">
+		<a href="category1.html">
+		    <img src="picture/img102.jpg" alt="Category 1">
+		    <p>هنر</p>
+		</a>
+	    </div>
+	    <div class="category">
+		<a href="category1.html">
+		    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
+		    <p>هنر</p>
+		</a>
+	    </div>
+	    <div class="category">
+		<a href="category1.html">
+		    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
+		    <p>هنر</p>
+		</a>
+	    </div>
+	    <div class="category">
+		<a href="category1.html">
+		    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
+		    <p>هنر</p>
+		</a>
+	    </div>
+	    <div class="category">
+		<a href="category1.html">
+		    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
+		    <p>هنر</p>
+		</a>
+	    </div>
+	    <div class="category">
+		<a href="category1.html">
+		    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
+		    <p>هنر</p>
+		</a>
+	    </div>
+	</div>
+	<div class="separator" style="margin-top: 50px;">
+	    <h1 class="hot-categories" style=" margin-top: 80px;">دسته های داغ</h1>
+	    <div class="categories" class="amir">
+<?php require "../view/hot-cate.php";
+foreach ($rows as $row) {
+	echo "<div class='category'>";
+	echo "<a href=category/$row[categoryid] >";
+	echo " <img src=' $row[categoryimage]'>";
+	echo $categories[] = $row['categoryname'];
+	echo " </a>  </div>";
+}
+?>
 
-                <button class="btn">سبد خرید</button>
-            </div>
-            <div class="right-items">
-                <form method="get" action="">
-                <input type="text" name="search" placeholder="جستجو..." id="searchInput">
-                <button type="submit" id="searchButton">جستجو</button>
-                 </form>
-                 <div class="search-results">
-             
-                </div>
-             
-                 
-                
-            </div>
-        </div>
-        <!-- <div class="second-row">
-            <a href="#" class="nav-item">دسته بندی</a>
-            <a href="#" class="nav-item">پیگیری سفارشات</a>
-            <a href="#" class="nav-item">درباره ما</a>
-        </div> -->
-        <img style="width: 100vw;    height: auto;    margin-top: 20px;" src="picture/1.jpg" alt="">
-        <div class="categories" class="amir" style="margin-top: 50px; ">
-            <div class="category">
-                <a href="category1.html">
-                    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
-                    <p>هنر</p>
-                </a>
-            </div>
-            <div class="category">
-                <a href="category1.html">
-                    <img src="picture/img102.jpg" alt="Category 1">
-                    <p>هنر</p>
-                </a>
-            </div>
-            <div class="category">
-                <a href="category1.html">
-                    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
-                    <p>هنر</p>
-                </a>
-            </div>
-            <div class="category">
-                <a href="category1.html">
-                    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
-                    <p>هنر</p>
-                </a>
-            </div>
-            <div class="category">
-                <a href="category1.html">
-                    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
-                    <p>هنر</p>
-                </a>
-            </div>
-            <div class="category">
-                <a href="category1.html">
-                    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
-                    <p>هنر</p>
-                </a>
-            </div>
-            <div class="category">
-                <a href="category1.html">
-                    <img src="picture/palette-svgrepo-com.svg" alt="Category 1">
-                    <p>هنر</p>
-                </a>
-            </div>
-        </div>
-        <div class="separator" style="margin-top: 50px;">
-            <h1 class="hot-categories" style=" margin-top: 80px;">دسته های داغ</h1>
-            <div class="categories" class="amir">
-                <?php require "../view/hot-cate.php";
-                foreach ($rows as $row) {
-                    echo "<div class='category'>";
-                    echo "<a href=category/$row[categoryid] >";
-                    echo " <img src=' $row[categoryimage]'>";
-                    echo $categories[] = $row['categoryname'];
-                    echo " </a>  </div>";
-                }
-                ?>
-            
-        </div>
+	</div>
 
-            <h1 class="hot-categories" style=" margin-top: 80px;">ایتم های داغ</h1>
-            <div class="categories" class="amir">
-            <?php
-            require"../view/hot-items.php";
-            foreach ($rows as $row) {
+	    <h1 class="hot-categories" style=" margin-top: 80px;">ایتم های داغ</h1>
+	    <div class="categories" class="amir">
+<?php
+require"../view/hot-items.php";
+foreach ($rows as $row) {
 
-                echo "<div class='category'>";
-                echo "<a href=''>";
-                echo " <img src='picture/palette-svgrepo-com.svg'>";
-               echo  $items[] = $row['name'] . "<br>" . $row['rating'] . "<br>";
-                echo " </a>  </div>";
+	echo "<div class='category'>";
+	echo "<a href=''>";
+	echo " <img src='picture/palette-svgrepo-com.svg'>";
+	echo  $items[] = $row['name'] . "<br>" . $row['rating'] . "<br>";
+	echo " </a>  </div>";
 
-            }
-            ?>
-            </div>
-        </div>
-        <footer class="footer">
-            <div class="footer-left">
-                <a href="#"><img src="picture/telegram-svgrepo-com.svg" alt="tel"></a>
-                <a href="#"><img src="picture/instagram-svgrepo-com.svg" alt="insta"></a>
-                <a href="#"><img src="picture/github-svgrepo-com.svg" alt="github"></a>
-            </div>
-            <div class="footer-center">
-                <a href="#" style="margin-bottom: 8px;">درباره ما</a>
-                <a href="#" style="margin-top: 8px;">خدمات مشتریان</a>
-            </div>
-            <div class="footer-right">
-                <p>شماره تماس: 12345678</p>
-                <p>amirhosseinfatemicod1385@gmail.com :ایمیل</p>
-                <p>آدرس: خیابان معلم ، معلم 53 ، بین قایم مقام 32 34</p>
-            </div>
-        </footer>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#searchInput').on('input', function() {
-                var searchTerm = $(this).val();
-                $.get('search.php', {search: searchTerm}, function(data) {
-                    $('.search-results').html(data);
-                });
-            });
-        });
-    </script>
-   
-     
+}
+?>
+	    </div>
+	</div>
+	<footer class="footer">
+	    <div class="footer-left">
+		<a href="#"><img src="picture/telegram-svgrepo-com.svg" alt="tel"></a>
+		<a href="#"><img src="picture/instagram-svgrepo-com.svg" alt="insta"></a>
+		<a href="#"><img src="picture/github-svgrepo-com.svg" alt="github"></a>
+	    </div>
+	    <div class="footer-center">
+		<a href="#" style="margin-bottom: 8px;">درباره ما</a>
+		<a href="#" style="margin-top: 8px;">خدمات مشتریان</a>
+	    </div>
+	    <div class="footer-right">
+		<p>شماره تماس: 12345678</p>
+		<p>amirhosseinfatemicod1385@gmail.com :ایمیل</p>
+		<p>آدرس: خیابان معلم ، معلم 53 ، بین قایم مقام 32 34</p>
+	    </div>
+	</footer>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function() {
+	$('#searchInput').on('input', function() {
+		var searchTerm = $(this).val();
+		$.get('search.php', {search: searchTerm}, function(data) {
+			$('.search-results').html(data);
+		});
+	});
+});
+</script>
+
+
 
 </body>
 
---
+++ view/navbar/nav.php
@@ -0,0 +1,32 @@
+<nav id=nav>
+	<input id=hamburger type=checkbox >
+	<span id=tb></span>
+	<span id=me></span>
+	<span id=bb></span>
+	<ul>
+		<li><a href="/">test</a></li>
+		<li><a href="/">test</a></li>
+		<li><a href="/">test</a></li>
+		<li><a href="/">test</a></li>
+		<li>
+		<?php if ($username == false) {
+		echo '<a class="btn" href="sign-up.php">ورود / ثبت نام </a>';
+		} else {
+		$husername = htmlentities($username);
+		echo '<a href="userpage/?=username"' . $husername . '>' . $husername . '</a>';
+		} ?>
+		</li>
+		<li>
+			<form id=searchbox method="get">
+				<input id=search type=text name=s placeholder=Search title="Search the shop">
+				<label>
+				<input type=submit style="display:none;">
+				<div id=searchbutton >
+				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 50 50">
+					<path d="M 21 4 C 11.082241 4 3 12.082241 3 22 C 3 31.917759 11.082241 40 21 40 C 24.62177 40 27.99231 38.91393 30.820312 37.0625 L 43.378906 49.621094 L 47.621094 45.378906 L 35.224609 32.982422 C 37.581469 29.938384 39 26.13473 39 22 C 39 12.082241 30.917759 4 21 4 z M 21 8 C 28.756241 8 35 14.243759 35 22 C 35 29.756241 28.756241 36 21 36 C 13.243759 36 7 29.756241 7 22 C 7 14.243759 13.243759 8 21 8 z"></path>
+				</svg>
+				</div>
+				</label>
+				 </form></li>
+	</ul>
+</nav>
